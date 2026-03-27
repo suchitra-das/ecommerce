745ec4da2f946f05556a84081a6c61ce7be4d157
suchitra-das <sdas90933@gmail.com>
2026-03-27 12:44:09 +0530

    added style

--- src/pages/DetailsPage.tsx
+++ src/pages/DetailsPage.tsx
@@ -1,6 +1,5 @@
 
 
-import { Button } from "@/components/ui/button"
 import { EcommerceContext, type dataType } from "@/context/ecommerceContext"
 import { useContext } from "react"
 import { useParams } from "react-router-dom"
